f818a87cfeb046edde997f9e7b65de978d131a00
meira.igor <meira.igor@gmail.com>
2020-05-14 22:05:25 -0300

    entity com lombok

---
+++ src/main/java/com/imeira/bookstoremanager/entity/Book.java
@@ -0,0 +1,40 @@
+package com.imeira.bookstoremanager.entity;
+
+import javax.persistence.*;
+import com.imeira.bookstoremanager.entity.Author;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
